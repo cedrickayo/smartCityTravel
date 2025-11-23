22f14382f7a9d94ed350c2f32339460a0ee5b061
cedrickayo <cedric.kayo@gmail.com>
2025-11-22 22:23:13 -0500

    modification du chemin des fichiers de test, suppression de s sur tests

--- test/test_simulation_read_from_kafka.py
+++ test/test_simulation_read_from_kafka.py
@@ -11,13 +11,15 @@ conn = SparkSession \
             .getOrCreate()
 
 @patch("pyspark.sql.streaming.DataStreamReader.load")
-def test_kafka_mock(mock_readstream):
+def test_kafka_mock(mock_load):
     #simulation de lecture du flux streaming depuis kafka
     #Au lieu de lire les données depuis kafka, spark va lire le fichier static local grace à la méthode patch
 
-    mock_readstream.return_value = conn.read.json("test/Data/trajet.json")
+    fake_df = conn.read.json("test/Data/trajet.json")
+
+    mock_load.return_value=fake_df
 
     df=read_data_from_kafka(conn,"trajet")
 
     #verification que le dataframe contient exactement 1 ligne comme dans le fichier trajet.json
-    assert df.count()== 1
+    assert df.count() == 1
