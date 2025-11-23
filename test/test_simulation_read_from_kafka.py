9fcc5ccd94217e86acc8f34f89b312f92db09b32
cedrickayo <cedric.kayo@gmail.com>
2025-11-22 22:15:10 -0500

    modification du chemin des fichiers de test, suppression de s sur tests

--- test/test_simulation_read_from_kafka.py
+++ test/test_simulation_read_from_kafka.py
@@ -10,7 +10,7 @@ conn = SparkSession \
             .master("local[*]") \
             .getOrCreate()
 
-@patch("comsummer.SparkSession.readStream")
+@patch("pyspark.sql.streaming.DataStreamReader.load")
 def test_kafka_mock(mock_readstream):
     #simulation de lecture du flux streaming depuis kafka
     #Au lieu de lire les données depuis kafka, spark va lire le fichier static local grace à la méthode patch
